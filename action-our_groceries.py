791c59e84fd79ec6fb881b8493f1407432a57432
Paul <paul@facestab.net>
2019-01-03 10:35:57 +0000

    Adding to list works

--- action-our_groceries.py
+++ action-our_groceries.py
@@ -3,6 +3,7 @@
 
 from hermes_python.hermes import Hermes
 import os
+import re
 from ourgroceriesclient import ourgroceriesclient
 from snipshelpers.thread_handler import ThreadHandler
 from snipshelpers.config_parser import SnipsConfigParser
@@ -13,7 +14,6 @@ CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI =  "config.ini"
 
 MQTT_IP_ADDR = "localhost"
-#MQTT_IP_ADDR = "192.168.86.2"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
@@ -27,18 +27,21 @@ class Skill_OurGroceries:
             config = None
         username = None
         password = None
+        print(config)
         if config and config.get('secret', None) is not None:
             if config.get('secret').get('username', None) is not None:
                 username = config.get('secret').get('username')
                 if username == "":
                     username = None
             if config.get('secret').get('password', None) is not None:
-                code = config.get('secret').get(password)
+                password = config.get('secret').get('password')
                 if password == "":
                     code = None
         if username is None or password is None:
-            print('No configuration')
+            print('Bad configuration')
         self.client = ourgroceriesclient.OurGroceriesClient(username, password)
+        self.default_list = config.get('global').get("defaultlist")
+        print("Default List is " + self.default_list)
         self.queue = Queue.Queue()
         self.thread_handler = ThreadHandler()
         self.thread_handler.run(target=self.start_blocking)
@@ -57,31 +60,43 @@ class Skill_OurGroceries:
         items = []
         if intent_message.slots.itemType:
             for item in intent_message.slots.itemType.all():
-                print(type(item.value))
                 items.append(item.value)
         return items
 
+    def extract_list(self, intent_message):
+        if intent_message.slots.listName:
+            for list_name in intent_message.slots.listName.all():
+                return list_name.value
+        return self.default_list
+    
     ####    section -> handlers of intents
     def callback(self, hermes, intent_message):
         print("[OurGroceries] Received")
-        ## all the intents have a house_room slot, extract here
-        if intent_message.intent.intent_name == 'addToList':
+        intent_name = intent_message.intent.intent_name
+        intent_name = re.sub(r'^\w+:', '', intent_name)
+        print(intent_name)
+        if intent_name == 'addToList':
             self.queue.put(self.add_to_list(hermes, intent_message))
         
     def add_to_list(self, hermes, intent_message):
         items = self.extract_items(intent_message)
+        list_name = self.extract_list(intent_message)
         if len(items) > 0:
             for item in items:
-                print(item)
-        self.terminate_feedback(hermes, intent_message)
+                self.client.add_item_to_list_by_name(list_name, item)
+
+        # if the list name doesn't already end in "list" add it for the purposes
+        # of speaking it
+        if re.match(' list$', list_name, re.I) == False:
+            list_name += ' List'
+            
+        text = 'Added ' + 'and'.join(items) + ' to the ' + list_name
+        
+        self.terminate_feedback(hermes, intent_message, text)
 
     ####    section -> feedback reply // future function
-    def terminate_feedback(self, hermes, intent_message, mode='default'):
-        if mode == 'default':
-            hermes.publish_end_session(intent_message.session_id, "")
-        else:
-            #### more design
-            hermes.publish_end_session(intent_message.session_id, "")
+    def terminate_feedback(self, hermes, intent_message, text=""):
+        hermes.publish_end_session(intent_message.session_id, text)
 
 if __name__ == "__main__":
     Skill_OurGroceries()
