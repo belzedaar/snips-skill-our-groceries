e69b0b650d657a5be0d7627ceb7af8c41f996074
Paul <paul@facestab.net>
2019-01-03 11:28:34 +1100

    Initial commit

---
+++ action-our_groceries.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from hermes_python.hermes import Hermes
+import os
+from ourgroceriesclient import OurGroceriesClient
+from snipshelpers.thread_handler import ThreadHandler
+from snipshelpers.config_parser import SnipsConfigParser
+import Queue
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+
+CONFIG_INI =  "config.ini"
+
+MQTT_IP_ADDR = "localhost"
+#MQTT_IP_ADDR = "192.168.86.2"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+_id = "snips-skill-our-groceries"
+
+class Skill_OurGroceries:
+    def __init__(self):
+        try:
+            config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+        except :
+            config = None
+        username = None
+        password = None
+        if config and config.get('secret', None) is not None:
+            if config.get('secret').get('username', None) is not None:
+                username = config.get('secret').get('username')
+                if username == "":
+                    username = None
+            if config.get('secret').get('password', None) is not None:
+                code = config.get('secret').get(password)
+                if password == "":
+                    code = None
+        if username is None or password is None:
+            print('No configuration')
+        self.client = OurGroceriesClient(username, password)
+        self.queue = Queue.Queue()
+        self.thread_handler = ThreadHandler()
+        self.thread_handler.run(target=self.start_blocking)
+        self.thread_handler.start_run_loop()
+
+    def start_blocking(self, run_event):
+        while run_event.is_set():
+            try:
+                self.queue.get(False)
+            except Queue.Empty:
+                with Hermes(MQTT_ADDR) as h:
+                    h.subscribe_intents(self.callback).start()
+
+    ####    section -> extraction of slot value
+    def extract_items(self, intent_message):
+        items = []
+        if intent_message.slots.itemType:
+            for item in intent_message.slots.itemType.all():
+                print(type(item.value))
+                items.append(item.value)
+        return items
+
+    ####    section -> handlers of intents
+    def callback(self, hermes, intent_message):
+        print("[OurGroceries] Received")
+        ## all the intents have a house_room slot, extract here
+        if intent_message.intent.intent_name == 'addToList':
+            self.queue.put(self.add_to_list(hermes, intent_message))
+        
+    def add_to_list(self, hermes, intent_message):
+        items = self.extract_items(intent_message)
+        if len(items) > 0:
+            for item in items:
+                print(item)
+        self.terminate_feedback(hermes, intent_message)
+
+    ####    section -> feedback reply // future function
+    def terminate_feedback(self, hermes, intent_message, mode='default'):
+        if mode == 'default':
+            hermes.publish_end_session(intent_message.session_id, "")
+        else:
+            #### more design
+            hermes.publish_end_session(intent_message.session_id, "")
+
+if __name__ == "__main__":
+    Skill_OurGroceries()
---
+++ ourgroceriesclient/ourgroceriesclient.py
@@ -0,0 +1,128 @@
+# -*-: coding utf-8 -*-
+""" Client to talk to Our Groceries API. """
+
+import requests
+import json
+import sys
+import re
+
+class OurGroceriesClient:
+    """Client to talk to Our Groceries API"""
+
+    cookieName = 'ourgroceries-auth'
+    url = 'https://www.ourgroceries.com/your-lists/'
+
+    def __init__(self, username=None, password=None):
+        """ Initialisation.
+        
+        :param username: Our Groceries email 
+        :param password: Our Groceries password.
+        """
+        self.signedIn = False
+        self.username = username
+        payload = {
+            'emailAddress': username, 
+            'password': password, 
+            'action': 'sign-me-in', 
+            'staySignedIn': 'on' 
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+            'Referer': 'https://www.ourgroceries.com/sign-in',
+            'Origin': 'https://www.ourgroceries.com'
+        }
+        r = requests.post('https://www.ourgroceries.com/sign-in', payload, allow_redirects=False, headers=headers)
+        if r.status_code != 302:
+            return
+
+        self.signInCookie =  r.cookies.get_dict()[OurGroceriesClient.cookieName]
+        
+        r = requests.get(OurGroceriesClient.url, headers=headers, cookies=self.get_cookie())
+        match = re.search( r'var g_teamId = "([^"]+)"', r.text)
+        if not match:
+            return
+        
+        self.teamId = match.group(1)
+        self.signedIn = True
+        self.cachedListIds = self.get_list_ids()
+        
+    def get_overview(self):
+        """ Returns the overview - this is your list of lists, and your receipes. """
+        return self.exec_command("getOverview")
+    
+    def get_list_names(self):
+        """ Returns the list of shopping list names"""
+        json = self.get_overview()
+        if not json:
+            return False
+        
+        lists = json["shoppingLists"]
+        return [x["name"] for x in lists]
+    
+    def get_list_ids(self):
+        """ Returns a dict st of shopping list ids and names."""
+        json = self.get_overview()
+        if not json:
+            return False
+        
+        lists = json["shoppingLists"]
+        return dict((x["name"], x["id"]) for x in lists)
+        
+    def get_master_list(self):
+        """ Returns list of everything you've ever added to the lists"""
+        return self.exec_command('getMasterList', {'version' : ''})
+
+    def get_list(self, listId):
+        """ Returns a specific list by id"""
+        return self.exec_command('getList', {'listId': listId, 'version' : ''})
+
+    def add_item_to_list(self, listId, item, count = None):
+        """ Adds an item to a list. Returns item id."""
+        if count:
+            item += ' ('
+            item += str(count)
+            item += ')'
+
+        json = self.exec_command('insertItem', {'listId': listId, 'value' : item})
+        return json["itemId"]
+
+    def add_item_to_list_by_name(self, listName, item, count = None):
+        """ Adds an item to a list, looking up the list Id by name """
+        return self.add_item_to_list(self.cachedListIds[listName], item, count)
+
+    def delete_all_crossed_off_items(self, listId):
+        """ Deletes all the crossed off items from the list"""
+        return self.exec_command('deleteAllCrossedOffItems', {'listId': listId, 'version' : ''})
+
+    def delete_item(self, listId, itemId):
+        """ Delete an item from a list. """
+        return self.exec_command('deleteItem',  {'listId': listId, 'itemId' : itemId})
+
+    def exec_command(self, command, args = None):
+        """ Posts a named command. """
+        if not self.signedIn:
+            return False
+        
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+            'Referer': 'https://www.ourgroceries.com/your_lists',
+            'Origin': 'https://www.ourgroceries.com'
+        }
+
+        # We always send these args
+        json = { "command": command, "teamId" : self.teamId }
+        # Add the passed in additional args to the standard ones
+        if args:
+            json.update(args)
+
+        r = requests.post(OurGroceriesClient.url, json=json, headers=headers, cookies=self.get_cookie())
+        return r.json()
+        
+    def get_cookie(self):
+        return {OurGroceriesClient.cookieName : self.signInCookie}
+
+if __name__ == "__main__":
+    client = OurGroceriesClient(sys.argv[1], sys.argv[2])
+    print(client.cachedListIds)
+    print(client.add_item_to_list_by_name("Shopping List", "Apples"))
+
